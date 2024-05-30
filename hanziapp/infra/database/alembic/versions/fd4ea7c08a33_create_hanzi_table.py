aabfc0dc0952cf017aa7f7ad9b837b2f05140916
Vitor Barbarisi <vitorbarbarisi@gmail.com>
2024-05-30 13:40:48 -0300

    fix: check if exists in alembic migration

--- hanziapp/infra/database/alembic/versions/fd4ea7c08a33_create_hanzi_table.py
+++ hanziapp/infra/database/alembic/versions/fd4ea7c08a33_create_hanzi_table.py
@@ -5,11 +5,11 @@ Revises:
 Create Date: 2024-01-22 00:46:39.386686
 
 """
-from typing import Sequence, Union
-
-from alembic import op
 import sqlalchemy as sa
 
+from typing import Sequence, Union
+from alembic import op
+from sqlalchemy import Table, MetaData
 
 # revision identifiers, used by Alembic.
 revision: str = 'fd4ea7c08a33'
@@ -19,23 +19,26 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade():
-    op.create_table(
-        "hanzi",
-        sa.Column("character", sa.String(length=1), primary_key=True),
-        sa.Column("frequency", sa.Integer, nullable=False),
-        sa.Column("calls", sa.Integer, default=0),
-        sa.Column("meaning", sa.String, default=""),
-        sa.Column("decomposition", sa.String, default=""),
-        sa.Column("etymology", sa.String, default=""),
-        sa.Column("related_words", sa.String, default=""),
-        sa.CheckConstraint(
-            "length(character) = 1",
-            name=op.f("character_length"),
-        ),
-        sa.PrimaryKeyConstraint("character", name=op.f("pk_hanzi")),
-        if_not_exists=True
-    )
+    engine = op.get_bind()
+    metadata = MetaData(engine)
 
+    if not Table('hanzi', metadata, autoload=True).exists():
+        op.create_table(
+            "hanzi",
+            sa.Column("character", sa.String(length=1), primary_key=True),
+            sa.Column("frequency", sa.Integer, nullable=False),
+            sa.Column("calls", sa.Integer, default=0),
+            sa.Column("meaning", sa.String, default=""),
+            sa.Column("decomposition", sa.String, default=""),
+            sa.Column("etymology", sa.String, default=""),
+            sa.Column("related_words", sa.String, default=""),
+            sa.CheckConstraint(
+                "length(character) = 1",
+                name=op.f("character_length"),
+            ),
+            sa.PrimaryKeyConstraint("character", name=op.f("pk_hanzi")),
+            if_not_exists=True
+        )
 
 def downgrade():
     op.drop_table("hanzi")
